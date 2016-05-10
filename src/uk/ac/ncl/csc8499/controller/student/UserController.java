fa9ce2e907355dff486c18f51c113311f9c3f428
tommyjin <tommyjing0620@gmail.com>
2016-05-10 01:36:03 +0100

    add student/user

---
+++ src/uk/ac/ncl/csc8499/controller/student/UserController.java
@@ -0,0 +1,11 @@
+package uk.ac.ncl.csc8499.controller.student;
+
+import com.jfinal.core.Controller;
+import com.jfinal.ext.route.ControllerBind;
+
+/**
+ * Created by tommy on 2016/5/10.
+ */
+@ControllerBind(controllerKey = "/student/user")
+public class UserController extends Controller {
+}
